caef3af8cfc644ad1f0c6a33d57ca4be3ba4cb97
Jaimie Norman <jaimie.luke.norman@gmail.com>
2019-07-12 11:54:49 +0200

    removed some redundant functions such as posts

--- pyapp/models.py
+++ pyapp/models.py
@@ -17,8 +17,6 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(32), nullable=False,
                            default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    # backref is similar to adding another column to the post model and lazy loads the data in one go
-    posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires=1800):
         s = Serializer(current_app.config['SECRET_KEY'], expires)
@@ -38,19 +36,6 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False,
-                            default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        # Function to determine how the 'Post' will look when printed out
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-
 class StoreItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
@@ -63,32 +48,3 @@ class StoreItem(db.Model):
         return f"StoreItem('{self.name}', '{self.price}', '{self.image}')"
 
 
-class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(128), nullable=False)
-    last_name = db.Column(db.String(128), nullable=False)
-    marital_state = db.Column(db.String(16), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.Integer, nullable=False)
-    race = db.Column(db.String(128), nullable=False)
-    nationality = db.Column(db.String(128), nullable=False)
-    blood_group = db.Column(db.String(2), nullable=False)
-    organ_donor = db.Column(db.BOOLEAN, nullable=False)
-    disabilities = db.Column(db.String(128), nullable=False, default='None')
-    occupation = db.Column(db.String(128), nullable=False)
-    street_address = db.Column(db.String(128), nullable=False)
-    house_number = db.Column(db.Integer, nullable=False)
-    city = db.Column(db.String(128), nullable=False)
-    province = db.Column(db.String(128), nullable=False)
-    zip_code = db.Column(db.Integer, nullable=False)
-    progress_notes = db.relationship('ProgressNote', backref='progress_report', lazy=True)
-
-
-class ProgressNote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    hospital = db.Column(db.String(128), nullable=False)
-    ward = db.Column(db.String(100), nullable=False)
-    progress_note = db.Column(db.Text)
-    investigation = db.Column(db.Text)
-    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
--- pyapp/users/routes.py
+++ pyapp/users/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, url_for, redirect, flash, render_template, request
 from flask_login import current_user, login_user, logout_user, login_required
 
 from pyapp import db, bcrypt
-from pyapp.models import User, Post
+from pyapp.models import User
 from pyapp.users.forms import RegistrationForm, LoginForm, UpdateDetailsForm, RequestResetForm, ResetPasswordForm
 from pyapp.users.utils import save_image, send_reset_email
 
@@ -73,8 +73,7 @@ def account():
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('user_posts.html', posts=posts, user=user)
+    return render_template('user_posts.html', user=user)
 
 
 @users.route("/reset_password", methods=['GET', 'POST'])
